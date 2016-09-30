a3ad5f72ae0375e61d18b5e8bf918116eda1d7b2
cani <cani>
2016-09-30 14:38:25 +0200

    add an example for how to use gulp-angular-library-builder (also used for manuel testing builder)

---
+++ example/gulpfile.js
@@ -0,0 +1,16 @@
+/*jslint node: true */
+'use strict';
+
+var gulp = require('gulp');
+
+var options = {
+    module: 'my-demo',
+    name: 'my-demo',
+    hostHeader: 'localhost',
+   /* see all options in gulp-angular-library-builder index.js*/
+};
+
+
+require('gulp-angular-library-builder')(options);
+
+gulp.task('default', ['serve:dev']);
