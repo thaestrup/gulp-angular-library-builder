97a79477aca423b9b1c98dda11b88cec36d3603c
Jens Borch Christiansen <jens.borch@gmail.com>
2016-10-20 15:06:00 +0200

    Fixed error after dependency updates

--- src/ts.gulp.js
+++ src/ts.gulp.js
@@ -5,33 +5,31 @@ var gulp = require('gulp'),
     ts = require('gulp-typescript'),
     path = require('path');
 
-module.exports = function(options) {
+module.exports = function (options) {
 
     var defaults = {
-        compilerOptions: {
-            noImplicitAny: true,
-            target: "es5",
-            sourceMap: true,
-            declarationFiles: true,
-            noExternalResolve: false,
-            sortOutput: true,
-            removeComments: false,
-            preserveConstEnums: true
-        }
+        noImplicitAny: true,
+        target: "es5",
+        sourceMap: true,
+        declarationFiles: true,
+        noExternalResolve: false,
+        sortOutput: true,
+        removeComments: false,
+        preserveConstEnums: true
     };
 
     /**
      * Compile all typescript files and source maps from options.src and output them to options.dist
      */
-    gulp.task('ts', function() {
+    gulp.task('ts', function () {
         var tsProject = ts.createProject({
-                compilerOptions: defaults
-            }),
+            compilerOptions: defaults
+        }),
             tsResult = gulp.src(path.join(options.src, '**/*.ts'))
-            .pipe(sourcemaps.init())
-            .pipe(ts(tsProject)).js
-            .pipe(sourcemaps.write('maps'))
-            .pipe(gulp.dest(path.join(options.target, "ts")));
+                .pipe(sourcemaps.init())
+                .pipe(tsProject(tsProject)).js
+                .pipe(sourcemaps.write('maps'))
+                .pipe(gulp.dest(path.join(options.target, "ts")));
         return tsResult;
     });
 
